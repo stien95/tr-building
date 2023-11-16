f031c6e04110b17f7e0cc52718d138011040fc7f
stien95 <stivenvillegas258@gmail.com>
2023-11-15 20:50:32 -0500

    V1.13

--- functions/prisma.js
+++
@@ -1,9 +0,0 @@
-// prisma.js
-
-const { PrismaClient } = require('@prisma/client');
-
-// Inicializar una única instancia de PrismaClient
-const prisma = new PrismaClient();
-
-// Exportar la instancia de Prisma para que pueda ser utilizada en otras partes de tu aplicación
-module.exports = { prisma };
